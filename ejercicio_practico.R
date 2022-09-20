f016221a5f86d59103e61b9c10aee8633296bb43
carmerlun <carmerlun@gmail.com>
2022-09-20 18:17:00 +0200

    añado fotos y algo de codigo en mi parte CARLOS

--- ejercicio_practico.R
+++ ejercicio_practico.R
@@ -100,3 +100,7 @@ m<-lm(x,y)
 ############################################
 CARLOS
 
+# The length_1_mm variable is the total or snout-fork length for cutthroat trout (total length from 1987 - 1994; snout-fork length since 1995), and snout-vent length for salamanders (all in millimeters).
+levels(ourdata$section)
+str(ourdata)
+ourdata$section <- as.factor(ourdata$section)
