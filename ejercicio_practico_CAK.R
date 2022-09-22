984a3d1a3a4343610048e6ceecdcdb90242d96ef
xtnaak <113282891+xtnaak@users.noreply.github.com>
2022-09-22 15:00:00 +0200

    cosillas

--- ejercicio_practico_CAK.R
+++ ejercicio_practico_CAK.R
@@ -60,19 +60,38 @@ ggplot(data_species2, aes(x = length_1_mm,
 
 lm_lter1 <- glm(weight_g ~ length_1_mm + species, data=data_species2)
 summary(lm_lter1)
+check_model(lm_ter1)
 
 lm_lter2 <- lm(weight_g ~ length_1_mm * species, data=data_species2)
 summary(lm_lter2)
 check_model(lm_lter2)
 
-## prueba subset quitando juveniles
-data_species_adult <- ourdata %>%
-  subset(species == "Coastal giant salamander") %>%
-  subset(length_1_mm > 100)
+# Las asumpciones de los lm no se cumplen, vamos a trasformar los datos
+# alternativas: la funcion es exponencial, podriamos transformar solo la variable predictora =length
+# alternativa 2 hacer un log log (predictor y respuesta) este puede ser la mejor alternativa ya que
+# hay muchos datos de biomasa cercanos a cero, asi aumentas la escala
+# sugiero hacer log(y o x+1), para que no salgan valores negativos
 
+data_species2$log2_length <- log2(data_species2$length_1_mm)
+data_species2$log10_weight <- log10(data_species2$weight_g)
 
-data_species_adult$species <- as.factor(data_species_adult$species)
-ggplot(data_species_adult, aes(x = length_1_mm,
-                          y = weight_g,
-                          color = species)) +
+ourdata$species <- as.factor(ourdata$species)
+ggplot(ourdata, aes(x = length_1_mm,
+                    y = weight_g,
+                    color = species)) +
   geom_point()
+
+
+# Pruebas una vez resuelto el caso
+
+# ## prueba subset quitando juveniles
+# data_species_adult <- ourdata %>%
+#   subset(species == "Coastal giant salamander") %>%
+#   subset(length_1_mm > 100)
+#
+#
+# data_species_adult$species <- as.factor(data_species_adult$species)
+# ggplot(data_species_adult, aes(x = length_1_mm,
+#                           y = weight_g,
+#                           color = species)) +
+#   geom_point()
